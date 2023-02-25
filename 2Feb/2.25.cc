91042c204afc5954824e4c0222df5be3dd43929d
yyz845935161 <845935161@qq.com>
2023-02-25 12:38:44 +0000

    2.25日打卡 三题oj

---
+++ 2Feb/2.25.cc
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <vector>
+using namespace std;
+
+
+
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+    TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p== nullptr && q==nullptr) return true;
+        if(p == nullptr || q== nullptr) return false;
+        if(p->val != q->val) return false;
+
+        return isSameTree(p->left, q->left)&&isSameTree(p->right, q->right);
+    }
+};
+
+class Solution3 {
+public:
+
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p== nullptr && q==nullptr) return true;
+        if(p == nullptr || q== nullptr) return false;
+        if(p->val != q->val) return false;
+
+        return isSameTree(p->left, q->left)&&isSameTree(p->right, q->right);
+    }
+
+
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(root == nullptr) return false;
+        if(isSameTree(root, subRoot))
+        {
+            return true;
+        }
+        return isSubtree(root->left,subRoot) || isSubtree(root->right,subRoot);
+
+
+    }
+};
+
+
+class Solution2 {
+public:
+
+    bool isSymmetric2(TreeNode* p,TreeNode* q)
+    {
+        if(p== nullptr && q==nullptr) return true;
+        if(p == nullptr || q== nullptr) return false;
+        if(p->val != q->val) return false;
+        return isSymmetric2(p->left, q->right)&&isSymmetric2(p->right, q->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if(root == nullptr) return true;
+        return isSymmetric2(root->left,root->right);
+    }
+};
