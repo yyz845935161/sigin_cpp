5cad150b2d8b32388f264f5a327cf3ad6032319a
yyz845935161 <845935161@qq.com>
2023-03-07 09:12:36 +0000

    3.7日打卡  搜索树递归实现增删查

--- 3Mar/3.3.cpp
+++ 3Mar/3.3.cpp
@@ -14,27 +14,105 @@ using namespace std;
 //     return 0;
 // }
 
-class Person
-{
-public:
-    virtual void BuyTicket() { cout << "买票-全价" << endl; }
-};
-class Student : public Person
-{
-public:
-    virtual void BuyTicket() { cout << "买票-半价" << endl; }
-};
+// class Person
+// {
+// public:
+//     virtual void BuyTicket() { cout << "买票-全价" << endl; }
+// };
+// class Student : public Person
+// {
+// public:
+//     virtual void BuyTicket() { cout << "买票-半价" << endl; }
+// };
 
-void Func(Person &p)
+// void Func(Person &p)
 
-{
-    p.BuyTicket();
+// {
+//     p.BuyTicket();
+// }
+// int main()
+// {
+//     Person ps;
+//     Student st;
+//     Func(ps);
+//     Func(st);
+//     return 0;
+// }
+
+
+int main() {
+    string s;
+    string ans;
+    getline(cin, s);
+    signed int substri = s.rfind(" ");
+    int last_index = s.size();
+    while(substri)
+    {
+        for(int i=substri+1;i<last_index;i++){
+            // cout<<s[i];
+            ans+=s[i];
+        }
+        ans+=" ";
+        string temp(s.begin(),s.begin()+substri);
+        last_index =substri;
+        substri = temp.rfind(" ");
+        if(substri == -1)
+        {
+            break;;
+        }
+        
+    }
+
+    for(int i=substri+1;i<last_index;i++){
+            ans+=s[i];
+        }
+
+    cout<<ans;
+    return 0 ;
+    
 }
-int main()
+
+
+#include <iostream>
+using namespace std;
+#include <string>
+
+bool isAr(char a)
 {
-    Person ps;
-    Student st;
-    Func(ps);
-    Func(st);
+    if(a <= 'z' && a >= 'a' || a <= 'Z' && a >= 'A' )
+    return true;
+    else return false;
+}
+int Alobort() {
+    string s;
+    getline(cin,s);
+    string ans;
+    int begin=0;
+    int len=0;
+    int i = 0;
+    while(i< s.size()) {
+        if(isAr(s[i]))
+        {
+            i++;
+            continue;
+        }
+
+        else
+         {
+            begin = i;
+            while(!isAr(s[i]))
+            {
+                i++;
+            }
+            if(i - begin >len)
+            {
+                len = i-begin;
+            }
+            string temp(s.begin()+begin,s.begin()+i);
+            ans = temp;
+         }
+    
+    }
+    cout<<ans<<endl;
     return 0;
 }
--- 3Mar/3.6.cpp
+++ 3Mar/3.6.cpp
@@ -10,11 +10,10 @@ struct BSTreeNode
     BSTreeNode<K> *_right;
     K _key;
     /* data */
-    BSTreeNode(const K& key)
-        :_key(key)
-        ,_left(nullptr)
-        ,_right(nullptr)
-    {}
+    BSTreeNode(const K &key)
+        : _key(key), _left(nullptr), _right(nullptr)
+    {
+    }
 };
 
 template <class K>
@@ -26,112 +25,296 @@ struct BSTree
 public:
     BSTree()
         : _root(nullptr)
-    {}
+    {
+    }
 
-    
-
-    bool Insert(const K& key){
-        if(this->_root == nullptr)
+    bool Insert(const K &key)
+    {
+        if (this->_root == nullptr)
         {
             this->_root = new Node(key);
             return true;
         }
-        Node* parent = nullptr;
-        Node* cur = _root;
-        while (cur) 
+        Node *parent = nullptr;
+        Node *cur = _root;
+        while (cur)
         {
             /* code */
-            if(cur ->_key <key)
+            if (cur->_key < key)
             {
                 parent = cur;
-                cur=cur->_right;
+                cur = cur->_right;
             }
-            else if (cur ->_key > key)
+            else if (cur->_key > key)
             {
                 parent = cur;
-                cur=cur->_left;
+                cur = cur->_left;
             }
             else
             {
                 return false;
             }
-            
-    
-
         }
-        Node* temp = new Node(key); 
-        if(parent ->_key <key)
+        Node *temp = new Node(key);
+        if (parent->_key < key)
         {
             parent->_right = temp;
         }
-        else{
+        else
+        {
             parent->_left = temp;
         }
-
-
-
     }
 
+    bool InsertR(const K &key)
+    {
+        return _InsertR(_root, key);
+    }
+
+    Node *FindR(const K &key)
+    {
+        _FindR(_root, key);
+    }
     void InOrder()
     {
         _InOrder(this->_root);
+        cout << endl;
     }
 
-    void _InOrder(Node* node)
+    void _InOrder(Node *node)
     {
-        if(node == nullptr)
+        if (node == nullptr)
         {
             return;
         }
         _InOrder(node->_left);
-        cout<<node->_key<<"  ";
+        cout << node->_key << "  ";
         _InOrder(node->_right);
     }
-    
 
-    bool* Find(const K& key)
+    bool Find(const K &key)
     {
-        Node* cur = _root:
+        Node *cur = _root;
         while (cur)
         {
-            if(cur->key <key)
+            if (cur->key < key)
             {
                 cur = cur->_right;
             }
-            if(cur->key > key)
+            if (cur->key > key)
             {
                 cur = cur->_left;
             }
-            else{
+            else
+            {
                 return true;
             }
         }
         return false;
     }
 
-    bool Erase(const K& key)
+    bool Erase(const K &key)
+    {
+        Node *parent = nullptr;
+        Node *cur = this->_root;
+        while (cur)
+        {
+            if (cur->_key < key)
+            {
+                parent = cur;
+                cur = cur->_right;
+            }
+            if (cur->_key > key)
+            {
+                parent = cur;
+                cur = cur->_left;
+            }
+            else
+            {
+                // 找到，准备删除
+                // 如果左边为空
+                if (cur->_left == nullptr)
+                {
+                    if (parent == nullptr)
+                    {
+                        _root = cur->_right;
+                    }
+                    else
+                    {
+                        if (parent->_left == cur)
+                        {
+                            parent->_left = cur->_right;
+                        }
+                        else
+                        {
+                            parent->_right = cur->_right;
+                        }
+                    }
+
+                    delete cur;
+                    cur = nullptr;
+                }
+                // 如果左边为空
+                else if (cur->_right == nullptr)
+                {
+                    if (parent == nullptr)
+                    {
+                        _root = cur->_right;
+                    }
+                    else
+                    {
+                        if (parent->_left == cur)
+                        {
+                            parent->_left = cur->_left;
+                        }
+                        else
+                        {
+                            parent->_right = cur->_left;
+                        }
+                    }
+                    if (parent->_left == cur)
+                    {
+                        parent->_left = cur->_left;
+                    }
+                    else
+                    {
+                        parent->_right = cur->_left;
+                    }
+                    delete cur;
+                    cur = nullptr;
+                }
+                // 如果左右都不为空
+                else
+                {
+                    Node *father = cur;
+                    Node *max = cur->_left;
+                    while (max->_right)
+                    {
+                        father = max;
+                        max = max->_right;
+                    }
+                    cur->_key = max->_key;
+                    if (father->_left == max)
+                    {
+                        father->_left = max->_left;
+                    }
+                    else
+                    {
+                        father->_right = max->_left;
+                    }
+
+                    delete max;
+                    // min = nullptr;
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    bool EraseR(const K& key)
     {
 
     }
-
 
 
 private:
     Node *_root;
+
+    bool _InsertR(Node *&root, const K &key)
+    {
+        if (root == nullptr)
+        {
+            root = new Node(key);
+            return true;
+        }
+        if (root->_key < key)
+        {
+            return _InsertR(root->_right, key);
+        }
+        else if (root->_key > key)
+        {
+            return _InsertR(root->_left, key);
+        }
+        else
+            return false;
+    }
+
+    Node *_FindR(Node *root, const K &key)
+    {
+        if (root == nullptr)
+        {
+            return nullptr;
+        }
+        if (root->_key < key)
+        {
+            return _FindR(root->_right, key);
+        }
+        else if (root->_key > key)
+        {
+            return _FindR(root->_left, key);
+        }
+        else
+        {
+            return root;
+        }
+    }
+
+    bool _EraseR(Node *&root,const K& key)
+    {
+        if(root == nullptr)
+        {
+            return false;
+        }
+        if(root -> key < key)
+        {
+            _EraseR(root->_right,key);
+        }
+        else if(root -> key > key)
+        {
+            _EraseR(root->_left,key);
+        }
+        else
+        {
+            Node* del =root;
+            if(root->_left ==nullptr)
+            {
+                root = root ->_right;
+            }
+            else if(root->_right == nullptr)
+            {
+                root = root ->_left;
+            }
+            else
+            {
+                Node* min = root->_right;
+                while(min -> _left)
+                {
+                    min = min->_left;
+                }
+                swap(min->_key,root ->_key);
+                _EraseR(root->_right,_key)
+            }
+            delete del;
+            return true;
+
+        }
+    }
+
 };
-
-
-
-
 
 void TestBSTree()
 {
     BSTree<int> t;
-    int a[] = {5,3,4,1,7,8,2,6,0,9};
-    for(auto e:a)
+    int a[] = {5, 3, 4, 1, 7, 8, 2, 6, 0, 9};
+    for (auto e : a)
     {
-        t.Insert(e);
+        t.InsertR(e);
     }
+    t.InOrder();
+
+    t.Erase(5);
     t.InOrder();
 }
 
