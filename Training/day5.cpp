5d64accb00727f830744a3ef7d8fbcec418ac964
yyz845935161 <845935161@qq.com>
2023-03-22 04:28:23 +0000

    3.22日打卡

--- Training/day5.cpp
+++ Training/day5.cpp
@@ -33,3 +33,37 @@ int main() {
     
 }
 // 64 位输出请用 printf("%lld")
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    vector<int> v;
+    cin>>n;
+    v.reserve(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+    int ans=v[0];
+    int be = ans;
+    for(int i=1;i<n;i++)
+    {
+        if(be+v[i]>v[i])
+        {
+            be+=v[i];
+        }
+        else 
+        {
+            be=v[i];
+        }
+        if(be>ans) ans=be;
+    }
+    cout<<ans;
+
+    return 0;
+}
+// 64 位输出请用 printf("%lld")
