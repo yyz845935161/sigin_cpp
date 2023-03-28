d77bf5a3a827b43f2ab234fc0eb07f4527453734
yyz845935161 <845935161@qq.com>
2023-03-28 09:07:43 +0000

    3.28日打卡

--- Training/day24.cpp
+++ Training/day24.cpp
@@ -6,7 +6,7 @@
 //         {
 //             return board[i][j];
 //         }
-
+        
 //         if(i==5)
 //         {
 //             return depgetMost(board,i,j+1) + board[i][j];
@@ -20,143 +20,74 @@
 //             return max(depgetMost(board,i+1,j) + board[i][j],depgetMost(board,i,j+1) + board[i][j]);
 //         }
 //     }
-
+    
 //     int getMost(vector<vector<int> > board) {
 //         // write code here
-
+        
 //      return depgetMost(board,0,0);
 //     }
 // };
 
-// #include <iostream>
-// #include <vector>
-// using namespace std;
-// int ROW, COL;
-// vector<vector<int>> maze;
-// vector<vector<int>> path_tmp;  // 临时路劲
-// vector<vector<int>> path_best; // 最佳路劲
-// void MazeTrack(int i, int j)
-// {
-
-//     maze[i][j] = 1; // 代表（i,j）已经走过
-//     path_tmp.push_back({i, j});
-//     // 判断是否到达出口
-//     if (i == ROW - 1 && j == COL - 1)
-//     {
-//         // 寻找最短路劲
-//         if (path_best.empty() || path_best.size() > path_tmp.size())
-//             path_best = path_tmp;
-//     }
-//     // 向右走
-//     if (j + 1 < COL && maze[i][j + 1] == 0)
-//         MazeTrack(i, j + 1);
-//     // 向左走
-//     if (j - 1 >= 0 && maze[i][j - 1] == 0)
-//         MazeTrack(i, j - 1);
-//     // 向上走
-//     if (i - 1 >= 0 && maze[i - 1][j] == 0)
-//         MazeTrack(i - 1, j);
-//     // 向下走
-//     if (i + 1 < ROW && maze[i + 1][j] == 0)
-//         MazeTrack(i + 1, j);
-//     maze[i][j] = 0; // 回溯 恢复路劲
-//     path_tmp.pop_back();
-// }
-// int main()
-// {
-//     while (cin >> ROW >> COL)
-//     {
-//         maze = vector<vector<int>>(ROW, vector<int>(COL, 0)); // 开辟迷宫空间
-//         path_tmp.clear();
-//         path_best.clear();
-//         // 首先输入迷宫
-//         for (int i = 0; i < ROW; ++i)
-//         {
-//             for (int j = 0; j < COL; ++j)
-//                 cin >> maze[i][j];
-//         }
-//         MazeTrack(0, 0); // 从起始点（0，0）开始走
-//         // 输出路径
-//         for (int i = 0; i < path_best.size(); ++i)
-//         {
-//             cout << "(" << path_best[i][0] << "," << path_best[i][1] << ")" << endl;
-//         }
-//     }
-//     return 0;
-// }
-
-#include <iostream>
+#include<iostream>
+#include<vector>
 using namespace std;
-#include <vector>
+int n,m;
+vector<int> v;
+vector<vector<int>> vv;
+vector<vector<int>> temp_path;
+vector<vector<int>> best_path;
 
-vector<vector<int>> path_temp;
-
-void deepout(vector<vector<int>> &vv, int n, int m, int i, int j, vector<vector<int>> &ans, char bef)
+void casepath(int i,int j)
 {
-    if (i < 0 || j < 0 || i >= n || j >= m)
+    if(i>=n || i <0 || j>=m || j<0) return;
+    if(vv[i][j]==1) return;
+    vv[i][j]=0;
+    temp_path.push_back({i,j});
+    if(i==n-1 && j==m-1)
     {
-        return;
+        if(!temp_path.empty() && best_path.size()>temp_path.size())
+        {
+            best_path=temp_path;
+        }
     }
-    if (vv[i][j] == 1)
-    {
-        return;
-    }
-    vv[i][j] = 1;
-    path_temp.push_back({i, j});
-    if (i == n - 1 && j == m - 1) // 寻找最佳路径
-    {
-        // 寻找最佳路径
-        if (ans.empty() || ans.size() > path_temp.size())
-            ans = path_temp;
-    }
-
-    // if (bef != 'd')
-        deepout(vv, n, m, i + 1, j, ans, 'u');
-    // if (bef != 'u')
-        deepout(vv, n, m, i - 1, j, ans, 'd');
-    // if (bef != 'r')
-        deepout(vv, n, m, i, j + 1, ans, 'l');
-    // if (bef != 'l')
-        deepout(vv, n, m, i, j - 1, ans, 'r');
-
-    vv[i][j] = 0; // 回溯
-    path_temp.pop_back();
+    
+    casepath(i+1,j);
+    casepath(i,j+1);
+    casepath(i-1,j);
+    casepath(i,j-1);
+    vv[i][j]=1;
+    temp_path.pop_back();
 }
+
+
 
 int main()
 {
-    int n, m;
-    while (cin >> n >> m)
+    
+    while(cin>>n>>m)  
     {
-        vector<vector<int>> vv;
-        vector<int> v(m);
-        vv.resize(n, v);
-        vector<vector<int>> ans;
-
-        path_temp.clear();
-        for (int i = 0; i < n; i++)
+        vv.resize(n,vector<int>(m,0));
+        temp_path.clear();
+        best_path.clear();
+        for(int i=0;i<n;i++)
         {
-            for (int j = 0; j < m; j++)
+            for(int j=0;j<m;j++)
             {
-                cin >> vv[i][j];
+                cin>>vv[i][j];
             }
         }
-        // for (auto& e:vv)
-        // {
-        //     for (auto a:e) {
-        //         cout << a << ' ';
-        //     }
-        //     cout<<endl;
-            
-        // }
-            // cout<<'-';
-        deepout(vv, n, m, 0, 0, ans, 'r');
 
-        for (int i = 0; i < ans.size(); i++)
+
+        casepath(0,0);
+        
+        for(auto e:vv)
         {
-
-            cout << '(' << ans[i][0] << ',' << ans[i][1] << ')' << endl;
+            cout<<'('<<e[0]<<','<<e[1]<<')'<<endl;
         }
+        
+         return 0;       
     }
-    return 0;
+    
+    
+    
 }
