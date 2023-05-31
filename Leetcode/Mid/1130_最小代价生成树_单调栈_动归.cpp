776237eac8ecca48be3ea7481660cf75079ab360
yyz845935161 <845935161@qq.com>
2023-05-31 13:33:10 +0000

    5.31日打卡

---
+++ Leetcode/Mid/1130_最小代价生成树_单调栈_动归.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int mctFromLeafValues(vector<int>& arr) {
+        int n = arr.size();
+        string a = "asdb";
+        vector<vector<int>> dp(n, vector<int>(n,  INT16_MAX / 4)), mval(n, vector<int>(n));
+        for (int j = 0; j < n; j++) {
+            mval[j][j] = arr[j];
+            dp[j][j] = 0;
+            for (int i = j - 1; i >= 0; i--) {
+                mval[i][j] = max(arr[i], mval[i + 1][j]);
+                for (int k = i; k < j; k++) {
+                    dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j] + mval[i][k] * mval[k + 1][j]);
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+};
+
+
+int main()
+{
+    Solution a;
+    vector<int> arr={6,2,4};
+    a.mctFromLeafValues(arr);
+}
