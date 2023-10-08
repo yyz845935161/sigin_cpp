4a97b36cf411b195dc597e95e6c8d099df0eebaa
yyz845935161 <845935161@qq.com>
2023-10-08 12:01:39 +0800

    10.8日打卡

--- test.cpp
+++ test.cpp
@@ -1,48 +1,29 @@
 #include<iostream>
 using namespace std;
 
-class A
-{
-public:
-    virtual void func1()
-    {
-        cout<<"父类的func1"<<endl;
-    }
-    void func2()
-    {
-        cout<<*(long long*)this<<endl;
-    }
-};
 
-class B:public A
+
+void bigOrSmall()
 {
-public:
-    virtual void func1()
+    int i = 1;
+    char *p = (char*)&i;
+    if(*p == 1)
     {
-        cout<<"子类的func1"<<endl;
+        cout<<"小端"<<endl;
     }
-    void func2()
+    else
     {
-        cout<<*(long long*)this<<endl;
+        cout<<"大端"<<endl;
     }
-};
+
+
+}
 
 
 int main()
 {
-    cout<<sizeof(long long)<<endl;
 
-    A a;
-    B b;
-    A* aa = new B;
-    aa->func1();
-    aa->func2();
-    a.func2();
-    B b2;
-    b.func2();
-    b2.func2();
-
-
-
+    bigOrSmall();
     return 0;
+    
 }
